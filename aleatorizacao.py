174ed722e447900d51fc33cc9d8e89f6a9630fbf
Adelgrin <50162397+Adelgrin@users.noreply.github.com>
2025-04-29 19:09:01 +0000

    validar

--- aleatorizacao.py
+++ aleatorizacao.py
@@ -1,1 +1,76 @@
 import faker
+
+def gerar_nome_filme_serie():
+    fake = faker.Faker()
+    # Gera um nome aleatório de filme combinando palavras
+    nome_filme = f"{fake.word().capitalize()} {fake.word().capitalize()}"
+    return nome_filme
+
+def gerar_genero_filme():
+    generos = [
+        "Ação", "Comédia", "Drama", "Fantasia", "Terror", 
+        "Ficção Científica", "Romance", "Suspense", "Animação", "Documentário"
+    ]
+    fake = faker.Faker()
+    return fake.random_element(generos)
+
+def gerar_dados_pessoa():
+    fake = faker.Faker()
+    nome = fake.name()
+    email = fake.email()
+    data_nascimento = fake.date_of_birth(minimum_age=18, maximum_age=40)
+    return {"nome": nome, "email": email, "data_nascimento": data_nascimento}
+
+def gerar_avaliacao(nomeF,usuario,tipo):
+    fake = faker.Faker()
+    comentario = fake.sentence(nb_words=10)
+    data = fake.date_this_year()
+    nota = fake.random_int(min=1, max=5)
+    #usuario = fake.name()
+    #if fake.boolean():  # Decide aleatoriamente entre filme ou série
+        #tipo = "filme"
+        #nome = gerar_nome_filme_serie()
+    #else:
+        #tipo = "serie"
+        #nome = gerar_nome_filme_serie()
+    return {
+        "comentario": comentario,
+        "data": data,
+        "nota": nota,
+        "usuario": usuario,
+        "tipo": tipo,
+        "nome": nomeF
+    }
+if __name__ == "__main__":
+    pessoas = []
+    filmes = []
+    series = []
+    midias = []
+    avaliacoes = []
+    assinaturas = []
+    #todos os dados a serem inseridos no banco
+    assinex = ["premium", "basico", "familia"]
+    for _ in range(10):
+        pessoas.append(gerar_dados_pessoa())
+    for _ in range(5):
+        series.append(gerar_nome_filme_serie())
+        filmes.append(gerar_nome_filme_serie())
+    midias = filmes[:] + series[:]
+    for i in midias:
+        for j in pessoas:
+            if i in filmes:
+                tipo = "filme"
+            else:
+                tipo = "serie"
+            avaliacao = gerar_avaliacao(i, j["nome"],tipo)
+            #print(f"Avaliação para {avaliacao['tipo']} '{avaliacao['nome']}' por {avaliacao['usuario']}:")
+            #print(f"Comentário: {avaliacao['comentario']}")
+            #print(f"Data: {avaliacao['data']}")
+            #print(f"Nota: {avaliacao['nota']}")
+            #print()
+            avaliacoes.append(avaliacao)
+    for l in pessoas:
+        assinaturas.append({"usuario": l["nome"], "tipo": faker.Faker().random_element(assinex), "data_renovacao": faker.Faker().date_this_year()})
+
+    
+    print(pessoas)
--- insercao.py
+++ insercao.py
@@ -8,26 +8,35 @@ key = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inl2
 # Inicializar o cliente do Supabase
 supabase: Client = create_client(url, key)
 
-def inserirFilme(titulo,duracao,ano,nomeGenero):
+def inserirFilme(titulo,duracao,ano,nomeGenero):#terceiro
     idgenero = supabase.table("generos").select("id_genero").eq("nome", nomeGenero).execute()
     response = supabase.table("filmes").insert({"titulo" : titulo, "duracao" : duracao, "ano_lancamento" : ano, "id_genero" : idgenero}).execute()
 
-def inserirGenero(descricao,nome):
+def inserirGenero(descricao,nome):#primeiro
     response = supabase.table("generos").insert({"nome" : nome, "descricao" : descricao}).execute()
 
-def inserirAvaliacao(comentario,data,nota,nomeusr,nomeSerie,nomeFilme):
+def inserirAvaliacao(comentario,data,nota,nomeusr,nomeSerie,nomeFilme):#quinto
     idusr = supabase.table("usuarios").select("id_usuario").eq("nome", nomeusr).execute()
     idserie = supabase.table("series").select("id_serie").eq("nome", nomeSerie).execute()
     idfilme = supabase.table("filmes").select("id_filme").eq("nome", nomeFilme).execute()
     response = supabase.table("avaliacoes").insert({"comentario" : comentario, "data_avaliacao" : data, "nota" : nota, "id_usuario" : idusr, "id_serie" : idserie, "id_filme" : idfilme}).execute()
 
-def inserirSerie(Ntemp,status,titulo,nomeGenero):
+def inserirSerie(Ntemp,status,titulo,nomeGenero):#segundo
     idgenero = supabase.table("generos").select("id_genero").eq("nome", nomeGenero).execute()
     response = supabase.table("series").insert({"numero_temporadas" : Ntemp, "status" : status, "titulo" : titulo, "id_genero" : idgenero}).execute()
 
-def inserirAssinatura(tipo,valor,dataRenov,nome):
+def inserirAssinatura(tipo,valor,dataRenov,nome):#sexto
     idusr = supabase.table("usuarios").select("id_usuarios").eq("nome", nome).execute()
     response = supabase.table("assinaturas").insert({"tipo_plano" : tipo, "valor" : valor, "data_renovacao" : dataRenov, "id_usuario" : idusr}).execute()
 
-def inserirUsuario(nome,data,email):
+def inserirUsuario(nome,data,email):#quarto
     response = supabase.table("usuarios").insert({"nome" : nome, "data_nascimento" : data, "email" : email}).execute()
+
+if __name__ == "__main__":
+    inserirGenero("acão e aventura", "ação")
+    inserirSerie(5, "em andamento", "The Witcher", "ação")
+    inserirFilme("Vingadores", 120, 2012, "ação")
+    inserirUsuario("Lucas", "2000-01-01", "email@generico.com")
+    inserirAvaliacao("muito bom", "2023-10-01", 5, "Lucas", "", "Vingadores")
+    inserirAssinatura("mensal", 29.90, "2023-10-01", "Lucas")
+    #dados para testar a escrita do banco de dados
