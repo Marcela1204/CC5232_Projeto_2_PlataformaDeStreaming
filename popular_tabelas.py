50abda7bc890a943456030d27225226c23426032
Adelgrin <50162397+Adelgrin@users.noreply.github.com>
2025-05-02 14:06:15 +0000

    aleatorizacao corrigido e funcionando

--- popular_tabelas.py
+++ popular_tabelas.py
@@ -1,49 +1,9 @@
 import faker
 import random
 from insercao import *
+from aleatorizacao import *
 
-def gerar_nome_filme_serie():
-    fake = faker.Faker()
-    # Gera um nome aleatório de filme combinando palavras
-    nome_filme = f"{fake.word().capitalize()} {fake.word().capitalize()}"
-    return nome_filme
-
-def gerar_genero_filme():
-    generos = [
-        "Ação", "Comédia", "Drama", "Fantasia", "Terror", 
-        "Ficção Científica", "Romance", "Suspense", "Animação", "Documentário"
-    ]
-    fake = faker.Faker()
-    return fake.random_element(generos)
-
-def gerar_dados_pessoa():
-    fake = faker.Faker()
-    nome = fake.name()
-    email = fake.email()
-    data_nascimento = fake.date_of_birth(minimum_age=18, maximum_age=40).strftime("%Y-%m-%d")
-    return {"nome": nome, "email": email, "data_nascimento": data_nascimento}
-
-def gerar_avaliacao(nomeF,usuario,tipo):
-    fake = faker.Faker()
-    comentario = fake.sentence(nb_words=10)
-    data = fake.date_this_year().strftime("%Y-%m-%d")
-    nota = fake.random_int(min=1, max=5)
-    #usuario = fake.name()
-    #if fake.boolean():  # Decide aleatoriamente entre filme ou série
-        #tipo = "filme"
-        #nome = gerar_nome_filme_serie()
-    #else:
-        #tipo = "serie"
-        #nome = gerar_nome_filme_serie()
-    return {
-        "comentario": comentario,
-        "data": data,
-        "nota": nota,
-        "usuario": usuario,
-        "tipo": tipo,
-        "nome": nomeF
-    }
-if __name__ == "__main__":
+def popular(numUsuarios,numMidias):
     pessoas = []
     filmes = []
     series = []
@@ -53,12 +13,22 @@ if __name__ == "__main__":
     #todos os dados a serem inseridos no banco
     assinex = ["premium", "basico", "familia"]
     assvalores = {"premium": 49.90, "basico": 29.90, "familia": 39.90}
-    for _ in range(10):
+    progresso = 0
+    print("indexando usuarios")
+    for _ in range(numUsuarios):
         pessoas.append(gerar_dados_pessoa())
-    for _ in range(5):
+        progresso += 1
+        print("{0}%".format(progresso/numUsuarios * 100), end="\r")
+    progresso = 0
+    print("indexando midias")
+    for _ in range(numMidias//2):
         series.append(gerar_nome_filme_serie())
         filmes.append(gerar_nome_filme_serie())
+        progresso += 1
+        print("{0}%".format(progresso/(numMidias//2) * 100), end="\r")
     midias = filmes[:] + series[:]
+    progresso = 0
+    print("indexando avaliacoes")
     for i in midias:
         for j in pessoas:
             if i in filmes:
@@ -72,9 +42,15 @@ if __name__ == "__main__":
             #print(f"Nota: {avaliacao['nota']}")
             #print()
             avaliacoes.append(avaliacao)
+            progresso = 0
+            print("{0}%".format(progresso/len(midias)*len(pessoas) * 100), end="\r")
+    progresso = 0
+    print("indexando assinaturas")
     for l in pessoas:
         tipo = faker.Faker().random_element(assinex)
         assinaturas.append({"usuario": l["nome"], "tipo": tipo, "data_renovacao": faker.Faker().date_this_year().strftime("%Y-%m-%d"), "valor" : assvalores[tipo]})
+        progresso += 1
+        print("{0}%".format(progresso/len(pessoas) * 100), end="\r")
 
     
     #print(pessoas)
@@ -122,4 +98,6 @@ if __name__ == "__main__":
         inserirAssinatura(i["tipo"], i["valor"], i["data_renovacao"], i["usuario"])
         progresso += 1
         print("{0}%".format(progresso/len(assinaturas) * 100), end="\r")
-    
+
+if __name__ == "__main__":
+    popular(10,20)#numero de usuarios(30) e numero de midias(50)
