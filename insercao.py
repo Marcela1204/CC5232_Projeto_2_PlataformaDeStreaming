15328235a4327ed6fb13a12a26bedf36f21b2017
Adelgrin <50162397+Adelgrin@users.noreply.github.com>
2025-05-02 13:03:05 +0000

    insercao corrigida e validada

--- insercao.py
+++ insercao.py
@@ -1,4 +1,5 @@
 from supabase import *
+import datetime
 # Credenciais do Supabase
 
 # Credenciais do Supabase
@@ -10,33 +11,37 @@ supabase: Client = create_client(url, key)
 
 def inserirFilme(titulo,duracao,ano,nomeGenero):#terceiro
     idgenero = supabase.table("generos").select("id_genero").eq("nome", nomeGenero).execute()
-    response = supabase.table("filmes").insert({"titulo" : titulo, "duracao" : duracao, "ano_lancamento" : ano, "id_genero" : idgenero}).execute()
+    response = supabase.table("filmes").insert({"titulo" : titulo, "duracao" : duracao, "ano_lancamento" : ano, "id_genero" : idgenero.data[0]["id_genero"]}).execute()
 
 def inserirGenero(descricao,nome):#primeiro
     response = supabase.table("generos").insert({"nome" : nome, "descricao" : descricao}).execute()
 
 def inserirAvaliacao(comentario,data,nota,nomeusr,nomeSerie,nomeFilme):#quinto
     idusr = supabase.table("usuarios").select("id_usuario").eq("nome", nomeusr).execute()
-    idserie = supabase.table("series").select("id_serie").eq("nome", nomeSerie).execute()
-    idfilme = supabase.table("filmes").select("id_filme").eq("nome", nomeFilme).execute()
-    response = supabase.table("avaliacoes").insert({"comentario" : comentario, "data_avaliacao" : data, "nota" : nota, "id_usuario" : idusr, "id_serie" : idserie, "id_filme" : idfilme}).execute()
+    idserie = supabase.table("series").select("id_serie").eq("titulo", nomeSerie).execute()
+    idfilme = supabase.table("filmes").select("id_filme").eq("titulo", nomeFilme).execute()
+    if idserie.data == []:
+        response = supabase.table("avaliacoes").insert({"comentario" : comentario, "data_avaliacao" : data, "nota" : nota, "id_usuario" : idusr.data[0]["id_usuario"], "id_filme" : idfilme.data[0]["id_filme"]}).execute()
+    else:
+        response = supabase.table("avaliacoes").insert({"comentario" : comentario, "data_avaliacao" : data, "nota" : nota, "id_usuario" : idusr.data[0]["id_usuario"], "id_serie" : idserie.data[0]["id_serie"]}).execute()
 
 def inserirSerie(Ntemp,status,titulo,nomeGenero):#segundo
     idgenero = supabase.table("generos").select("id_genero").eq("nome", nomeGenero).execute()
-    response = supabase.table("series").insert({"numero_temporadas" : Ntemp, "status" : status, "titulo" : titulo, "id_genero" : idgenero}).execute()
+    response = supabase.table("series").insert({"numero_temporadas" : Ntemp, "status" : status, "titulo" : titulo, "id_genero" : idgenero.data[0]["id_genero"]}).execute()
 
 def inserirAssinatura(tipo,valor,dataRenov,nome):#sexto
-    idusr = supabase.table("usuarios").select("id_usuarios").eq("nome", nome).execute()
-    response = supabase.table("assinaturas").insert({"tipo_plano" : tipo, "valor" : valor, "data_renovacao" : dataRenov, "id_usuario" : idusr}).execute()
+    idusr = supabase.table("usuarios").select("id_usuario").eq("nome", nome).execute()
+    response = supabase.table("assinaturas").insert({"tipo_plano" : tipo, "valor" : valor, "data_renovacao" : dataRenov, "id_usuario" : idusr.data[0]["id_usuario"]}).execute()
 
 def inserirUsuario(nome,data,email):#quarto
     response = supabase.table("usuarios").insert({"nome" : nome, "data_nascimento" : data, "email" : email}).execute()
 
 if __name__ == "__main__":
     inserirGenero("acão e aventura", "ação")
-    inserirSerie(5, "em andamento", "The Witcher", "ação")
+    inserirSerie(5, "ativa", "The Witcher", "ação")
     inserirFilme("Vingadores", 120, 2012, "ação")
-    inserirUsuario("Lucas", "2000-01-01", "email@generico.com")
-    inserirAvaliacao("muito bom", "2023-10-01", 5, "Lucas", "", "Vingadores")
-    inserirAssinatura("mensal", 29.90, "2023-10-01", "Lucas")
+    inserirUsuario("Lucas", datetime.datetime.strptime("2000-01-01","%Y-%m-%d").strftime("%Y-%m-%d"), "email@generico.com")
+    inserirAvaliacao("muito bom", datetime.datetime.strptime("2023-10-01","%Y-%m-%d").strftime("%Y-%m-%d"), 5, "Lucas", "", "Vingadores")
+    inserirAssinatura("mensal", 29.90, datetime.datetime.strptime("2023-10-01","%Y-%m-%d").strftime("%Y-%m-%d"), "Lucas")
+    #data_obj = datetime.strptime(data_string, "%Y-%m-%d").date()
     #dados para testar a escrita do banco de dados
